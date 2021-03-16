f9b4fac292eae444771470d48a5648d428561516
prashantpanwar2504 <prashantpanwar423361@gmail.com>
2021-03-16 06:23:35 -0700

    commit

---
+++ Data Structure  & Algorithim/2. Array/23. N_bonacci(Window sliding tech).cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+
+using namespace std;
+
+
+//Totally Done by me(My Solution).
+//Time Colexity O(n).
+//Based on Window Sliding Techniqe
+int N_bonacci(int arr[],int size, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (i == n-1)
+        {
+            arr[i] = 1;
+        }
+        else{
+            arr[i] = 0;
+        }
+    }
+    int curr_sum = arr[n-1];
+    int start = 0;
+    for (int j = n; j < size; j++)
+    {
+        if(j < size)
+        {
+            arr[j] = curr_sum;
+        }
+        if(start < (size-n))
+        {
+            curr_sum -= arr[start];
+            start++;
+        }
+        curr_sum += arr[j];
+    }
+    for(int k = 0; k<size ; k++)
+    {
+        cout << arr[k] << " ";
+    }
+}
+
+
+int main()
+{
+    int size;
+    int n;
+    cout << "Enter the Size: ";
+    cin >> size;
+    int arr[size];
+    cout << "Enter the term's n: ";
+    cin >> n;
+    N_bonacci(arr, size, n);
+    return 0;
+}
