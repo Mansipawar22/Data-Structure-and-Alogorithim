358d20f371c3560ab6da9c86727310cbe097ceac
prashantpanwar2504 <prashantpanwar423361@gmail.com>
2021-06-07 19:00:30 +0530

    matrix

---
+++ Data Structure  & Algorithim/6. Matrix/14. Rotate a Matrix by 90.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+
+
+using namespace std;
+
+void print(vector<int> arr[], int m, int n)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void Rotate_90(vector<int> arr[], int m, int n)
+{
+    int temp[m][n];
+    for(int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            temp[n-j-1][i] = arr[i][j];
+        }
+    }
+    for(int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            arr[i][j] = temp[i][j];
+        }
+    }
+}
+
+
+void Rotate_90_efficient(vector<int> arr[], int m, int n)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = i+1; j < n; j++)
+        {
+            swap(arr[j][i], arr[i][j]);
+        }
+    }
+    
+    for (int i = 0; i < m; i++)
+    {
+        int low = 0;
+        int high = n-1;
+        while (low < high)
+        {
+            swap(arr[low][i],arr[high][i]);
+            low++;
+            high--;
+        }
+        
+    }
+    
+}
+
+int main()
+{
+    int m;
+    int n;
+    cout << "Enter Row number:";
+    cin >> m;
+    cout << "Enter Column number:";
+    cin >> n;
+    vector<int> arr[m];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            int temp;
+            cin >> temp;
+            arr[i].push_back(temp);
+        }
+    }
+    print(arr, m, n);
+    Rotate_90_efficient(arr, m, n);
+    print(arr, m, n);
+    return 0;
+}
